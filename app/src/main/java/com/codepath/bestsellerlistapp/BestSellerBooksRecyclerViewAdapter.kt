8e670e35c4a08ed05a68d0aaa5c4ee5c476b7f4b
Samikshya_poudel <50699869+poudel4samikshya@users.noreply.github.com>
2023-02-11 22:33:28 -0500

    changed

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -1,10 +1,16 @@
 package com.codepath.bestsellerlistapp
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.R.id
 
 /**
@@ -14,9 +20,9 @@ import com.codepath.bestsellerlistapp.R.id
 class BestSellerBooksRecyclerViewAdapter(
     private val books: List<BestSellerBook>,
     private val mListener: OnListFragmentInteractionListener?
-    )
+)
     : RecyclerView.Adapter<BestSellerBooksRecyclerViewAdapter.BookViewHolder>()
-    {
+{
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.fragment_best_seller_book, parent, false)
@@ -31,6 +37,12 @@ class BestSellerBooksRecyclerViewAdapter(
         var mItem: BestSellerBook? = null
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mRanking: TextView = mView.findViewById<View>(id.ranking) as TextView
+        val mBookImage: ImageView = mView.findViewById<View>(id.book_image) as ImageView
+        val mBookDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mBuyButton: Button = mView.findViewById<Button>(id.buy_button) as Button
+
+
 
         override fun toString(): String {
             return mBookTitle.toString() + " '" + mBookAuthor.text + "'"
@@ -46,11 +58,22 @@ class BestSellerBooksRecyclerViewAdapter(
         holder.mItem = book
         holder.mBookTitle.text = book.title
         holder.mBookAuthor.text = book.author
+        holder.mRanking.text=book.rank.toString()
+        //holder.mBookImage
+        holder.mBookDescription.text= book.description
+        Glide.with(holder.mView)
+            .load(book.bookImageUrl)
+            .centerInside()
+            .into(holder.mBookImage)
 
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
                 mListener?.onItemClick(book)
             }
+        }
+        holder.mBuyButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(book.amazonUrl))
+            startActivity(it.context, browserIntent, null)
         }
     }
 
